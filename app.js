8955bf75897dbba1ba810855fafa824f68dd9636
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2022-03-16 11:33:30 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import express from 'express';
+
+const app = express();
+
+app.get('/catinfo', (req, res) => {
+  const cat = {
+    name: 'Frank',
+    birthdate: '2010-12-25',
+    weight: 5,
+  };
+  res.json(cat);
+});
+
+app.listen(3000, () => console.log(`listening on 3000`));
